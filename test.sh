4f49238dc5da795f4aac08a5dacbc154a50251df
Francisco <pimonty@example.com>
2017-11-24 04:03:22 +0000

    Msg: Failed building JavaScript bundle. (Solved: Babel config)

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm test
+
+# En Package.json tiene que contener lo siguiente:
+#
+#"scripts": {
+#    "test": "node_modules/.bin/jest"
+#  },
+#  "jest": {
+#    "preset": "jest-expo"
+#  }
